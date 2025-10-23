61b5f29b7457af08f40298c7afa121fde12fddbe
emieng <emilia.engberg@readspeaker.com>
2025-10-23 11:40:57 +0200

    Add missing namespace to ExtensionUtility constant

--- ext_localconf.php
+++ ext_localconf.php
@@ -10,8 +10,5 @@ defined("TYPO3") || die("Access denied.");
 	[
 		\Readspeaker\ReadspeakerServices\Controller\ReadspeakerServicesController::class => "",
 	],
-	ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
+	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
 );
-
-
-
